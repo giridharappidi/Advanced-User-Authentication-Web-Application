71e379324570a2ace905800704a987837876c5fd
giridharappidi <giridharsworld7@gmail.com>
2026-08-11 04:29:20 +0530

    Migrate real frontend source into flat structure, remove stray duplicate folders, fix CI workflow path

--- Advanced-User-Authentication-Web-Application-main-OLD/backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, MyTokenView
-from rest_framework_simplejwt.views import TokenRefreshView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('token/', MyTokenView.as_view(), name='token'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
